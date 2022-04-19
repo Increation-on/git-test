7413fee3736346a0a8c8759891261020be74883d
Maksim Dudarev <maxim.d132213@gmail.com>
2022-04-19 16:02:39 +0300

    deleete something

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ function App() {
       <p>1.Create new branch</p>
       <p>2.Push another changes</p>
       <p>3.changes</p>
-      <p>4. another changes</p>
-      <p>5.anothher changes</p>
       <p>READY for Merge</p>
       <p>another merge</p>
+      <p>delete something</p>
     </div>
   );
 }
